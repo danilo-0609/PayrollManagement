ca0b36372d94b4cdab36cd315fff1fbe44025cf8
Danilo Úsuga <daniusugaj@gmail.com>
2023-09-09 20:27:00 -0500

    Implementación parcial de jwt en el sistema

--- PayrollManagement.Domain/Users/IUserRepository.cs
+++ PayrollManagement.Domain/Users/IUserRepository.cs
@@ -5,7 +5,7 @@
         Task<List<User>> GetAllUsersAsync();
         Task<User> GetUserByIdAsync(UserId userId);
         Task<bool> UserExistsAsync(UserId userId);
-        void DeleteUser(UserId userId);
+        void DeleteUser(User userId);
         void UpdateUser(UserId userId, User user);
         void CreateUser(User user);
     }
